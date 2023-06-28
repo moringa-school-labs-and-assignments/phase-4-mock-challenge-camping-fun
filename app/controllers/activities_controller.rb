b94be87040411f65108b680963325bf233078b93
joshhoneypot <joshuaangugo@gmail.com>
2023-06-28 17:11:59 +0300

    camper serializer set

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,4 +3,14 @@ class ActivitiesController < ApplicationController
         activities = Activity.all  
         render json: activities, status: :ok
     end
+
+    def destroy
+        activity = Activity.find_by(id: params[:id])
+        if activity
+          activity.destroy
+          head :no_content
+        else
+          head :not_found
+        end
+    end
 end
